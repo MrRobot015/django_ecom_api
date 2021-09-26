8107f1993c3402d05ab65e01ffbfc710195c0fb5
Mujtaba Salah <mujtabasalah97@gmail.com>
2021-09-26 02:12:49 +0200

    configure user val1

--- ecom_api/api/user/models.py
+++ ecom_api/api/user/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=100, default='Anonymous')
+    email = models.EmailField(max_length=250, unique=True)
+    username = None
+
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+
+    session_token = models.CharField(max_length=10, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
