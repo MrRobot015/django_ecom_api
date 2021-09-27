57a6fef9fcee3b23fb5b79a22fb973f796b982c6
Mujtaba Salah <mujtabasalah97@gmail.com>
2021-09-27 05:05:58 +0200

    configure Order app v1

--- ecom_api/api/order/models.py
+++ ecom_api/api/order/models.py
@@ -1,3 +1,25 @@
+from functools import total_ordering
 from django.db import models
+from api.user.models import CustomUser
+from api.product.models import Product
+import uuid
 
 # Create your models here.
+
+class Order(models.Model):
+    user= models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
+    product_names = models.ManyToManyField('product', blank=True)
+    total_count = models.CharField(max_length=500, default=0)
+    transaction_id = models.UUIDField(default=uuid.uuid3, unique=True, editable=False)
+    total_amount =  models.IntegerField(default=0 , null=True , blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    @property
+    def getTotalCount(self):
+        """this method update the the review total and ratio after submiting a new review"""
+        total_amount = self.product_names.count()
+        self.total_amount = total_amount
+        self.save()
--- ecom_api/api/order/views.py
+++ ecom_api/api/order/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from .serializers import OrderSerializer
+from .models import Order
+
 
 # Create your views here.
